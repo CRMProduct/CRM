3061928a0befa5c719c271b13659ed6574af8c70
GeJingneng <1026685753@qq.com>
2017-10-13 08:57:40 +0800

    分页

--- crmStaff/src/com/ibm/crm/biz/IDeptBiz.java
+++ crmStaff/src/com/ibm/crm/biz/IDeptBiz.java
@@ -1,6 +1,5 @@
 package com.ibm.crm.biz;
 
-<<<<<<< HEAD
 import java.util.Map;
 
 import com.ibm.crm.base.PageBean;
@@ -10,15 +9,4 @@ public interface IDeptBiz {
 	//分页
 	public PageBean queryByPage(int pageCode, int pageSize, Map map);
 
-=======
-import com.ibm.vo.Crmdepartment;
-
-public interface IDeptBiz {
-
-	//根据Id查部门
-	public Crmdepartment srhDeptById(int depId);
-	
-	//更新部门信息
-	public Crmdepartment modifyDept(Crmdepartment crmDepartment);
->>>>>>> 795f20e72cac21aa9ffa1b102163771bd621ac0b
 }
--- crmStaff/src/com/ibm/crm/dao/IDeptDao.java
+++ crmStaff/src/com/ibm/crm/dao/IDeptDao.java
@@ -1,6 +1,5 @@
 package com.ibm.crm.dao;
 
-<<<<<<< HEAD
 import java.util.Map;
 
 import com.ibm.crm.base.GenericDao;
@@ -12,15 +11,4 @@ public interface IDeptDao extends GenericDao<Crmdepartment, Integer> {
 	//分页
 	public PageBean findByPage(int pageCode, int pageSize, Map map);
 
-=======
-import com.ibm.crm.base.GenericDao;
-import com.ibm.vo.Crmdepartment;
-
-public interface IDeptDao extends GenericDao<Crmdepartment, Integer> {
-	//根据Id查找部门
-	public Crmdepartment findByDeptId(int depId);
-	
-	//更新部门信息
-	public Crmdepartment updateDept(Crmdepartment crmDepartment);
->>>>>>> 795f20e72cac21aa9ffa1b102163771bd621ac0b
 }
